f441c0ba0d2a0513c88219a1d9d3a5dc67c88dcd
jciarelli <jciarelli@gmail.com>
2016-12-14 18:39:57 -0500

    Added triangle vectors and animated them

--- style.js
+++ style.js
@@ -1,4 +1,8 @@
 $(document).ready(function (){
+
+	$('.pink').hide(0).delay(400).fadeIn(2000);
+	$('.blue').hide(0).delay(300).fadeIn(1000);
+
 	$(window).scroll(function(){
 		if ($(this).scrollTop() >450) {
 			$(".navbar").fadeIn(700);
@@ -7,3 +11,5 @@ $(document).ready(function (){
 		}
 	});
 });
+
+
