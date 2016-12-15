0778a8080997fcee8453a1a59e24317ae82a8e09
jciarelli <jciarelli@gmail.com>
2016-12-15 14:42:01 -0500

    Added descriptions to projects and added Waypoints and animation to skills and projects

--- style.js
+++ style.js
@@ -1,7 +1,40 @@
-$(document).ready(function (){
+const left = document.getElementsByClassName("left");
+const right = document.getElementsByClassName("right");
 
-	$('.pink').hide(0).delay(400).fadeIn(2000);
-	$('.blue').hide(0).delay(300).fadeIn(1000);
+$(document).ready(function (){
+	// Skills title fading in
+	$(".skills-title").css('opacity', 0);
+
+	$(".skills-title").waypoint(function() {
+		$(".skills-title").addClass('fadeIn');
+		}, {offset: '80%'});
+
+	// Images of code skills fading in and up
+	$(".code-logos").css('opacity', 0);
+
+	$(".code-logos").waypoint(function() {
+		$(".code-logos").addClass('fadeInUp');
+		}, {offset: '70%'});
+
+	// Projects fading in from the left
+	for (let i=0; i < left.length; i++) {
+	$(left[i]).css('opacity', 0);
+
+	$(left[i]).waypoint(function() {
+		$(left[i]).addClass('fadeInLeft');
+		}, {offset: '70%'});
+	}
+	// Projects fading in from the right
+	for (let i=0; i < right.length; i++) {
+	$(right[i]).css('opacity', 0);
+
+	$(right[i]).waypoint(function() {
+		$(right[i]).addClass('fadeInRight');
+		}, {offset: '70%'});
+	}
+});	
+
+$(document).ready(function (){
 
 	$(window).scroll(function(){
 		if ($(this).scrollTop() >450) {
